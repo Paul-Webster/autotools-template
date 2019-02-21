c3cefced9175500682f883b31d52afdac57fb42f
Paul-Webster <campaw@aol.com>
2019-02-21 08:05:11 -0500

    Miscellanous cleanup.

--- src/libdmf/initialize.c
+++ src/libdmf/initialize.c
@@ -1,8 +1,5 @@
 /*=============================================================================
- * Name        : initialize.c
- * Author      : Paul Webster
- * Version     : 1.0
- * Copyright   : Copyright (C) 2018-2019
+ * initialize.c
  *============================================================================*/
 
 #include <crtdefs.h>
@@ -21,12 +18,15 @@ int dmf_initialize() {
  *============================================================================*/
 int dmf_is64bit() {
 	int reg_size = sizeof(size_t);
+
 	if (reg_size == 8)
 		puts("[Running 64 bit mode]");
 	else if (reg_size == 4)
 		puts("[Running 32 bit mode]");
+
 	printf("sizeof(int) [%d]\n", (int) sizeof(int));
 	printf("sizeof(long) [%d]\n", (int) sizeof(long));
+
 	return (reg_size == 4);
 }
 
